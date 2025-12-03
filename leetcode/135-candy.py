04c8517873f72e498317796dcbd79712f9ba0b56
manishnangare <nangaremanish@gmail.com>
2025-12-03 16:20:41 +0530

    135-py

---
+++ leetcode/135-candy.py
@@ -0,0 +1,19 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        
+        arr = [1] * len(ratings)
+
+        for i in range( 1, len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                arr[i] = arr[i-1]+1
+        
+        for i in range( len(ratings)-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                arr[i] = max( arr[i], arr[i+1]+1)
+
+        count = 0
+
+        for i in arr:
+            count += i
+
+        return count
