a46b29be7b5a7754817d4bd14648e2c61be5ce70
manishnangare <nangaremanish@gmail.com>
2026-01-11 00:32:50 +0530

    258-cpp

---
+++ leetcode/258-add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num/10 == 0)
+            return num;
+        else
+        {
+            int x = 0;
+            while(num>0)
+            {
+                x += num%10;
+                num /= 10;
+            }
+            return addDigits(x);
+        }
+    }
+};
